8d717eb3be85cfc130b7790b0cbf7f2bffec75eb
Viarani <via.damarani@gmail.com>
2026-01-20 10:08:13 +0700

    revisi grafik

--- app.py
+++ app.py
@@ -7,21 +7,20 @@ import os
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# 1. LOAD MODEL DAN DATA
+# LOAD MODEL DAN DATA
 model = joblib.load(os.path.join(BASE_DIR, 'model_salary_predictor.pkl'))
 df = pd.read_csv(os.path.join(BASE_DIR, 'salaries.csv'))
 
-# 2. FUNGSI OTOMATIS KONVERSI KODE NEGARA KE NAMA
+# KONVERSI KODE NEGARA KE NAMA
 def get_country_name(code):
     country = pycountry.countries.get(alpha_2=code)
     return country.name if country else code
 
-# 3. BUAT MAPPING LOKASI DARI DATASET (OTOMATIS)
-# Mengambil semua kode unik dari kolom residence dan company_location
+# MAPPING LOKASI DARI DATASET
 all_country_codes = set(df['employee_residence'].unique().tolist() + df['company_location'].unique().tolist())
 location_mapping = {code: get_country_name(code) for code in all_country_codes}
 
-# 4. OPSI DROPDOWN
+# OPSI DROPDOWN
 FORM_OPTIONS = {
     'work_year': sorted(df['work_year'].unique().tolist(), reverse=True),
     'experience_level': sorted(df['experience_level'].unique().tolist()),
@@ -33,7 +32,7 @@ FORM_OPTIONS = {
     'company_size': sorted(df['company_size'].unique().tolist())
 }
 
-# 5. MAPPING LABEL UI
+# MAPPING LABEL UI
 UI_LABELS = {
     'experience_level': {'EN': 'Entry-level / Junior', 'MI': 'Mid-level / Intermediate', 'SE': 'Senior-level', 'EX': 'Executive / Director'},
     'employment_type': {'FT': 'Full-time', 'PT': 'Part-time', 'CT': 'Contract', 'FL': 'Freelance'},
@@ -55,20 +54,17 @@ def predict():
         features = ['work_year', 'experience_level', 'employment_type', 'job_title', 
                     'employee_residence', 'remote_ratio', 'company_location', 'company_size']
         
-        # 2. Buat DataFrame
+        # 2. DataFrame
         input_df = pd.DataFrame([data])[features]
 
-        # 3. MAPPING STRING KE ANGKA (Label Encoding Manual)
-        # Kita harus mengubah semua kolom kategori menjadi angka agar model tidak error float
+        # 3. Label Encoding String ke Angka
         category_cols = ['experience_level', 'employment_type', 'job_title', 
                          'employee_residence', 'company_location', 'company_size']
         
         for col in category_cols:
-            # Menggunakan .hash() adalah cara cepat jika kamu tidak punya file LabelEncoder
-            # Tapi cara terbaik adalah menyesuaikan dengan angka di Notebook kamu
             input_df[col] = input_df[col].apply(lambda x: abs(hash(x)) % 1000) 
 
-        # 4. PREDIKSI
+        # 4. Model Prediksi
         prediction = model.predict(input_df)[0]
         return jsonify({'result': round(prediction, 2)})
         
